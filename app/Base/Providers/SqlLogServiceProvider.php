e1650d5a413b19a577a3bc54c5d0eacee465ddde
takenaka1739 <takuya_takenaka@justem-soft.co.jp>
2025-07-09 11:59:23 +0900

    型チェック

--- app/Base/Providers/SqlLogServiceProvider.php
+++ app/Base/Providers/SqlLogServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Barryvdh\Debugbar\DataCollector\QueryCollector;
 use Barryvdh\Debugbar\DataFormatter\QueryFormatter;
+use Illuminate\Database\Events\QueryExecuted;
 
 class SqlLogServiceProvider extends ServiceProvider
 {
@@ -18,53 +19,46 @@ class SqlLogServiceProvider extends ServiceProvider
      */
     public function register()
     {
-      // .envを見てログ出力を行うかどうかを判別
-      if (env('APP_DEBUG') !== true) {
-          return;
-      }
+        if (env('APP_DEBUG') !== true) {
+            return;
+        }
 
-      $queryCollector = new QueryCollector();
-      $queryCollector->setDataFormatter(new QueryFormatter());
-      // bindを当て込むための設定
-      $queryCollector->setRenderSqlWithParams(true);
+        $queryCollector = new QueryCollector();
+        $queryCollector->setDataFormatter(new QueryFormatter());
+        $queryCollector->setRenderSqlWithParams(true);
 
-      $db = new DB();
-      $logOutPut = [];
+        $db = new DB();
+        $logOutPut = [];
 
-      // コンソール実行かどうか
-      if (App::runningInConsole()) {
-          $logOutPut['execType'] = 'console';
-          $logOutPut['execCommand'] = implode(' ', $_SERVER['argv']);
-      } else {
-          $logOutPut['execType'] = 'web';
-          $logOutPut['method'] = $_SERVER['REQUEST_METHOD'];
-          $logOutPut['URL'] = url()->full();
-      }
-      $db::listen(
-          function ($query, $bindings = null, $time = null, $connectionName = null) use ($logOutPut, $db, $queryCollector) {
-              if ($query instanceof \Illuminate\Database\Events\QueryExecuted) {
-                  $bindings = $query->bindings;
-                  $time = $query->time;
-                  $connection = $query->connection;
+        if (App::runningInConsole()) {
+            $logOutPut['execType'] = 'console';
+            $logOutPut['execCommand'] = implode(' ', $_SERVER['argv']);
+        } else {
+            $logOutPut['execType'] = 'web';
+            $logOutPut['method'] = $_SERVER['REQUEST_METHOD'];
+            $logOutPut['URL'] = url()->full();
+        }
 
-                  $query = $query->sql;
-              } else {
-                  $connection = $db->connection($connectionName);
-              }
+        $db::listen(function ($query, $bindings = null, $time = null, $connectionName = null) use ($logOutPut, $db, $queryCollector) {
+            if ($query instanceof QueryExecuted) {
+                $bindings = $query->bindings;
+                $time = $query->time;
+                $connection = $query->connection;
+                $query = $query->sql;
+            } else {
+                $connection = $db->connection($connectionName);
+            }
 
-              $queryCollector->addQuery((string) $query, $bindings, $time, $connection);
+            $queryCollector->addQuery((string) $query, $bindings, $time, $connection);
 
-              // SQLの実行情報を取得
-              $sqlExecCollect = $queryCollector->collect();
-              foreach ($sqlExecCollect['statements'] as $sqlStatement) {
-                  // ログに出力するデータを作成
-                  $logOutPut['sql'] = $sqlStatement['sql'];
-                  $logOutPut['duration_str'] = $sqlStatement['duration_str'];
-              }
-              // ログを加工しやすいようにJSONで出力
-              Log::channel('sqllog')->debug(json_encode($logOutPut, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
-          }
-      );
+            $sqlExecCollect = $queryCollector->collect();
+            foreach ($sqlExecCollect['statements'] as $sqlStatement) {
+                $logOutPut['sql'] = $sqlStatement['sql'];
+                $logOutPut['duration_str'] = $sqlStatement['duration_str'];
+            }
+
+            Log::channel('sqllog')->debug(json_encode($logOutPut, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+        });
     }
 
     /**
